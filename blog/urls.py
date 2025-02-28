4390199c5d811f4637b652cfa4fce776926af6ee
coni1027 <nikolabitoria@gmail.com>
2025-02-28 14:54:44 +0800

    Created models for blog app and migrated, modifed both app and site wide urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,2 +1,10 @@
 from django.urls import path
+from . import views
 
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('blog/articles', views.index, name='blog_articles'),
+    path('blog/article/<int:pk>', views.index, name='blog_solo_article')
+]
+
+app_name = 'blog'
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+from . import models
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Landing page")
