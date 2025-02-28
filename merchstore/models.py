3515ca6b112887653ae641c85f1ae52ca8897d55
jaren.xiu <jarenmarcelino76@gmail.com>
2025-02-28 14:28:01 +0800

    Added get_absolute_url in models.py

--- merchstore/models.py
+++ merchstore/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class ProductType(models.Model):
@@ -28,3 +29,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse("merchstore:product-detail", args=[self.pk])
