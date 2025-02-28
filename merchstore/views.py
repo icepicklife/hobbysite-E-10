86f0f4cb5dcf65f90a546db72835c130ddfc8293
jaren.xiu <jarenmarcelino76@gmail.com>
2025-02-28 14:15:45 +0800

    Updated app-wide urls.py and app-wide views.py

--- merchstore/views.py
+++ merchstore/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Product
 
-# Create your views here.
+
+class ProductListView(ListView):
+    model = Product
+    template_name = "product_list.html"
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "product_detail.html"
+
+
+def product_list(request):
+    products = Product.objects.all()
+    ctx = {"products": products}
+
+    return render(request, "product_list.html", ctx)
+
+
+def recipe_detail(request, pk):
+    ctx = {"product": Product.objects.get(pk=pk)}
+
+    return render(request, "product_detail.html", ctx)
