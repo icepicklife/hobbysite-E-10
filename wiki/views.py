7e2900bf2d980e0339f193cee581ae308bd7d621
p41g3 <carbonellpaige@gmail.com>
2025-03-07 12:25:48 +0800

    updated project urls

--- wiki/views.py
+++ wiki/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
+from .models import Article
 
-# Create your views here.
+class ArticleListView(ListView):
+    model = Article
+    template_name = "wiki/article_list.html"
+    context_object_name = "articles"
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = "wiki/article_detail.html"
+    context_object_name = "article"
