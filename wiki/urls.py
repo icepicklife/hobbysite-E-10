35aae8d480df145ceb9289493302c4a26d2b263a
p41g3 <carbonellpaige@gmail.com>
2025-05-02 15:58:01 +0800

    trying to merge with main

--- wiki/urls.py
+++ wiki/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from .views import ArticleListView, ArticleDetailView
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = "wiki"
 
 urlpatterns = [
     path("articles/", ArticleListView.as_view(), name="article_list"),
-    path("article/<int:pk>/", ArticleDetailView.as_view(),
-         name="article_detail"),
-]
+    path("article/<int:pk>/", ArticleDetailView.as_view(),name="article_detail"),
+    # path("article/add/"),
+    # path("article/<int:pk>/edit/")
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- wiki/views.py
+++ wiki/views.py
@@ -1,30 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import ListView, DetailView
-from . import models
-
-
-def article_list(request):
-    article_list = models.Article.objects.all()
-    ctx = {"articles": article_list}
-
-    return render(request, "wiki/article_list.html", ctx)
-
-
-def article(request, pk):
-    article = models.Article.objects.get(pk=pk)
-    ctx = {"article": article}
-
-    return render(request, "wiki/article_detail.html", ctx)
+from .models import Article
+from accounts.models import UserAccount
 
 
 class ArticleListView(ListView):
-    model = models.Article
+    model = Article
     template_name = "wiki/article_list.html"
     context_object_name = "articles"
 
+    def get_queryset(self):
+        return super().get_queryset().order_by("category__name", "title", "-created_on")
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+
+        if user.is_authenticated:
+            try:
+                profile = UserAccount.objects.get(user=user)
+                user_articles = Article.objects.filter(author=profile)
+                other_articles = Article.objects.exclude(author=profile)
+            except UserAccount.DoesNotExist:
+                user_articles = None
+                other_articles = Article.objects.all()
+            
+            context["user_articles"] = user_articles
+            context["all_articles"] = other_articles
+        else:
+            context["user_articles"] = None
+            context["all_articles"] = Article.objects.all()
+
+        return context
+
 
 class ArticleDetailView(DetailView):
-    model = models.Article
+    model = Article
     template_name = "wiki/article_detail.html"
     context_object_name = "article"
