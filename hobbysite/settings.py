18d9e3716651dc38de628bd28160da7b1ee4a4b7
p41g3 <carbonellpaige@gmail.com>
2025-05-13 18:03:08 +0800

    fixed ALLOWED_HOSTS again

--- hobbysite/settings.py
+++ hobbysite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['your-digitalocean-domain.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['hobbysite-app-z78m7.ondigitalocean.app', 'localhost', '127.0.0.1']
 
 # Application definition
 
