7d69687945ace706a4f192540cf32ee0598095a3
p41g3 <carbonellpaige@gmail.com>
2025-05-13 17:45:44 +0800

    removed a slash in ALLOWED_HOSTS

--- hobbysite/settings.py
+++ hobbysite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['hobbysite-app-z78m7.ondigitalocean.app/']
+ALLOWED_HOSTS = ['hobbysite-app-z78m7.ondigitalocean.app']
 
 # Application definition
 
