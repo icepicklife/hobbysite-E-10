b6a0b34b5245162634e438380b7f1b82f80b1dc4
jaren.xiu <jarenmarcelino76@gmail.com>
2025-05-14 12:55:20 +0800

    Fixed password management

--- user_management/forms.py
+++ user_management/forms.py
@@ -1,16 +1,14 @@
 from django import forms
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
 
 User = get_user_model()
 
-class UserForm(forms.ModelForm):
+class UserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ["username", "password"]
-        widgets = {
-            "password": forms.PasswordInput(),
-        }
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
 
 class ProfileForm(forms.ModelForm):
     class Meta:
